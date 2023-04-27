fd41e35b91719347a37838c6e55b804ab8f5eb66
Eli <elipalmer2004@gmail.com>
2023-04-27 15:14:22 -0500

    Integreation

--- terrarium.py
+++ terrarium.py
@@ -3,26 +3,50 @@
 ################################################
 
 # MOISTURE SENSOR CODE
+from classes import Hardware
+from classes import TimeKeeper as TK
 import RPi.GPIO as GPIO
 import time
  
-#GPIO SETUP
-channel = 21
+#SENSOR SETUP
+wschannel = 13
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(channel, GPIO.IN)
+GPIO.setup(wschannel, GPIO.IN)
+
+#PUMP SETUP
+SECONDS_TO_WATER = 10
+RELAY = Hardware.Relay(12, False)
  
-def callback(channel):
-        if GPIO.input(channel):
-                print "Your plants have plenty of fresh water!"
+def waterpoll(wschannel):
+        if GPIO.input(wschannel):
+                print("Your plants have plenty of fresh water!")
+                return True
         else:
-                print "Your plants are dry!"
+                print("Your plants are dry!")
+                return False
+
+# DA PUMP CODE
+
+
+def water_plant(relay, seconds):
+    relay.on()
+    print("Plant is being watered!")
+    time.sleep(seconds)
+    print("Watering is finished!")
+    relay.off()
+
+def main():
+    test = 1
+    #time_keeper = TK.TimeKeeper(TK.TimeKeeper.get_current_time())
+    if(waterpoll(wschannel) == False):
+        print("Watering Initiated!")
+        water_plant(RELAY, SECONDS_TO_WATER)
+
  
-GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)  # let us know when the pin goes HIGH or LOW
-GPIO.add_event_callback(channel, callback)  # assign function to GPIO PIN, Run function on change
+#GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)  # let us know when the pin goes HIGH or LOW
+#GPIO.add_event_callback(channel, callback)  # assign function to GPIO PIN, Run function on change
  
 # infinite loop
-while True:
-        time.sleep(1)
 
   
   
@@ -41,6 +65,9 @@ import board
 dht = adafruit_dht.DHT22(board.D4)
 
 while True:
+    time.sleep(1)
+    main()
+
     try:
         temperature = dht.temperature
         humidity = dht.humidity
